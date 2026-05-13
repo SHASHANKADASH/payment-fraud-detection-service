165e5becedcb6d6fe13d4af6f0e7d876dffb7d2e
SHASHANKADASH <shibutom216@gmail.com>
2026-05-13 23:07:12 +0530

    [pfds-1]
    Initial commit

---
+++ src/main/java/org/shashanka/PaymentFraudDetectionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.shashanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentFraudDetectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentFraudDetectionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shashanka/PaymentFraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shashanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentFraudDetectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
